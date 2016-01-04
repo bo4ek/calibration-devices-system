aaf4c48ea43a07e7f24416e510bdf6a6ebaca5c1
bashenskii <bashenskii@gmail.com>
2016-01-04 22:52:25 +0200

    #429 fix localization in Provider role

--- web/src/main/webapp/resources/app/provider/controllers/GraficEmployeeProvider.js
+++ web/src/main/webapp/resources/app/provider/controllers/GraficEmployeeProvider.js
@@ -60,7 +60,7 @@ angular
                             tooltip: {
                                 headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                                '<td style="padding:0"><b>{point.y:.1f} опрацьовано заявок</b></td></tr>',
+                                '<td style="padding:0"><b>{point.y:.1f}'+' '+ $filter('translate')('VERIFICATIONS_PROCESSED') + '</b></td></tr>',
                                 footerFormat: '</table>',
                                 shared: true,
                                 useHTML: true
