064e0ec8bfeba8584853b47f16524f770303909c
bo4ek <bigbo4ek@gmail.com>
2016-01-13 16:57:17 +0200

    change BasicCertificate

--- service/src/main/java/com/softserve/edu/service/tool/ReportsService.java
+++ service/src/main/java/com/softserve/edu/service/tool/ReportsService.java
@@ -1,13 +1,11 @@
 package com.softserve.edu.service.tool;
 
-
 import com.softserve.edu.documents.parameter.FileFormat;
 import com.softserve.edu.documents.resources.DocumentType;
 import com.softserve.edu.service.utils.export.TableExportColumn;
 import org.apache.commons.vfs2.FileObject;
 
 import java.util.List;
-import java.util.Map;
 
 public interface ReportsService {
     FileObject buildFile(Long providerId, DocumentType documentType, FileFormat fileFormat) throws Exception;
