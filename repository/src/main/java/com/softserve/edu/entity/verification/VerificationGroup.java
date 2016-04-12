f37de209bad585db2b6f85435fe44c48aef53b2a
Yaroslav Skakun <skakun.jarik@gmail.com>
2016-04-12 22:28:03 +0300

    add files

---
+++ repository/src/main/java/com/softserve/edu/entity/verification/VerificationGroup.java
@@ -0,0 +1,47 @@
+package com.softserve.edu.entity.verification;
+
+import com.softserve.edu.entity.catalogue.Team.DisassemblyTeam;
+import com.softserve.edu.entity.device.CalibrationModule;
+import com.softserve.edu.entity.enumeration.verification.Status;
+import com.softserve.edu.entity.user.User;
+import com.softserve.edu.entity.verification.Verification;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "VERIFICATION_GROUP")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class VerificationGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Verification> verifications;
+
+    public VerificationGroup() {
+    }
+
+    public VerificationGroup(Set<Verification> verifications) {
+        this.verifications = verifications;
+    }
+
+
+    @Override
+    public String toString() {
+        return "VerificationGroup{" +
+                "id =" + id +
+                ", verifications =" + verifications +
+                '}';
+    }
+}
---
+++ web/src/main/webapp/resources/app/calibrator/controllers/EditAllUserVerificationsInTaskControllerCalibrator.js
@@ -0,0 +1,58 @@
+angular
+    .module('employeeModule')
+    .controller(
+        'EditAllUserVerificationsInTaskControllerCalibrator',
+        ['$scope', '$log', '$modalInstance',  '$rootScope', '$modal', 'ngTableParams',
+            'CalibrationTaskServiceCalibrator','$translate','$filter', 'toaster',
+        function ($scope, $log, $modalInstance, $rootScope, $modal,ngTableParams, CalibrationTaskServiceCalibrator, $translate, $filter, toaster) {
+
+            $scope.editAll = true;
+            $scope.verificationId = $rootScope.verifIDforEditing;
+
+                $rootScope.$on('$locationChangeStart', function () {
+                    $modalInstance.close();
+                });
+
+                $scope.cancel = function () {
+                    $modalInstance.close();
+                };
+
+                $scope.openEditVerificationModal = function () {
+                    $rootScope.editAll = $scope.editAll;
+                    var editVerificationModal = $modal.open({
+                        animation: true,
+                        controller: 'AddingVerificationsControllerCalibrator',
+                        templateUrl: 'resources/app/calibrator/views/modals/initiate-verification.html',
+                        size: 'lg'
+                    });
+                    editVerificationModal.result.then(function() {
+                        $scope.cancel();
+                        $scope.tableParams.reload();
+                    });
+                };
+            $scope.tableParams = new ngTableParams({
+                    page: 1,
+                    count: 100,
+                    sorting: {
+                        'queue': 'asc'
+                    }
+                },
+                {
+                    total: 0,
+                    getData: function ($defer, params) {
+                        var sortCriteria = Object.keys(params.sorting())[0];
+                        var sortOrder = params.sorting()[sortCriteria];
+                        CalibrationTaskServiceCalibrator.getVerificationsByTask(params.page(), params.count(),
+                                sortCriteria, sortOrder, $rootScope.taskId)
+                            .success(function (result) {
+                                $scope.resultsCount = result.totalItems;
+                                $defer.resolve(result.content);
+                                $scope.verifications = result.content;
+                                params.total(result.totalItems);
+                            }, function (result) {
+                                $log.debug('error fetching data:', result);
+                            });
+                    }
+                });
+
+            }]);
