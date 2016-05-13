32938d7a6c41fb6b740bd6ecd87ec5eab44d22e0
maksymmarych <Pelethebest80445746353>
2016-05-13 15:38:33 +0300

    Fix spring Transactions

--- service/src/main/java/com/softserve/edu/service/admin/impl/OrganizationServiceImpl.java
+++ service/src/main/java/com/softserve/edu/service/admin/impl/OrganizationServiceImpl.java
@@ -63,7 +63,7 @@ public class OrganizationServiceImpl implements OrganizationService {
     }
 
     @Override
-    @Transactional(readOnly = true)
+    @Transactional
     public void addOrganizationWithAdmin(String name, String email, String phone, List<String> types, List<String> counters,
                                          Integer employeesCapacity, Integer maxProcessTime, String firstName, String lastName,
                                          String middleName, String username, Address address, Address addressRegistered,
